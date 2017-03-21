1b9ad2afadb6c870b2feafccb976244c244d4524
Bernat Brunet Torruella <bernat@nan-tic.com>
2017-03-21 13:26:28 +0100

    Remove not needed code. Move it to csb58 module, that it's the only old modules active.

--- __init__.py
+++ __init__.py
@@ -2,19 +2,18 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-from .payment import *
+from . import payment
 
 def register():
     Pool.register(
-        BankAccount,
-        Journal,
-        Group,
-        PayLineStart,
-        ProcessPaymentStart,
-        CreatePaymentGroupStart,
+        payment.Journal,
+        payment.Group,
+        payment.PayLineStart,
+        payment.ProcessPaymentStart,
+        payment.CreatePaymentGroupStart,
         module='account_payment_es', type_='model')
     Pool.register(
-        PayLine,
-        ProcessPayment,
-        CreatePaymentGroup,
+        payment.PayLine,
+        payment.ProcessPayment,
+        payment.CreatePaymentGroup,
         module='account_payment_es', type_='wizard')
